44eed01942e03c75ef1d88fab734a5dea5f1d21a
gertelrina <gertelrina@yandex.ru>
2024-06-21 12:32:22 +0000

    init

---
+++ README.md
@@ -0,0 +1,52 @@
+# spark docker cluster
+To build:
+
+```
+sudo make build_base_image
+sudo make build_master_image
+sudo make build_slave_image
+sudo docker-compose up -d --scale slave=<slaves_number>
+```
+To stop:
+```
+sudo docker-compose down
+```
+
+Dataset: 
+Electric Vehicle Population Data
+```
+https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2/about_data
+https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD
+```
+
+Configs:
+block_size = 512m
+driver_mem = 4G
+
+To run: 
+In container 
+```
+cd ~/shared/
+
+# Optimized with partitioning and caching
+pyspark  --driver-memory 4G < spark_app_optimized.py
+
+# Not optimized
+pyspark  --driver-memory 4G < spark_app.py 
+```
+
+Results:
+Execution time
+| |non_optimized|optimized|
+|---|---|-----------|
+|1_slave|0:00:33.125732 |0:00:26.564846|
+|3_slave|0:00:30.492206 |0:00:24.235050|
+
+Ram consumption
+| |non_optimized|optimized|
+|---|---|-----------|
+|1_slave| 3533MB| 3207MB|
+|3_slave| 5865MB |5463MB|
+
+
+
---
+++ shared-master/spark_app.py
@@ -0,0 +1,43 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, ArrayType
+from pyspark.sql.functions import col,array_contains
+import datetime
+
+conf = (pyspark.SparkConf().setMaster("yarn"))
+sc = pyspark.SparkContext(conf = conf)
+sc._jsc.hadoopConfiguration().set("dfs.block.size", "8m")
+
+
+spark = SparkSession.builder.appName('Bench_app').getOrCreate()
+
+start =  datetime.datetime.now()
+
+print("1. ", datetime.datetime.now())
+df = spark.read.format('csv') \
+                .option('header',True) \
+                .option('multiLine', True) \
+                .load("hdfs:///input/dataset.csv")
+
+# enable partitioning
+print("2. ", datetime.datetime.now())
+
+print(df.count())
+df.printSchema()
+
+for ind in range(0,10):
+    df_models = df.groupBy([df["Model"], df["Make"]]).count()
+    # df_models.show()
+
+    df_count = df.join(df_models,"Model")
+    df_count.printSchema()
+    df_count.select(df_count["Model"],df_count["count"]).show()
+    df_count = df_count.orderBy(df_count["count"])
+    
+    #enable cache
+    df_2 = df_count.filter(df_count["count"] < 100).filter(df["Postal Code"].cast('int').alias('count2') % 2 == 0).groupBy(df_count["Model"], df_count["Vehicle Location"]).count()
+                    
+    df_count.join(df_2, "count" ,'inner').show()
+    print(f"Iter {ind} date {datetime.datetime.now()}")
+
+print ("total_time : ", datetime.datetime.now() - start)
